952e137a2195e0b72edfa7d94483eb5e805e8cdd
Rahma1225 <80515209+Rahma1225@users.noreply.github.com>
2024-03-25 00:20:09 +0100

    acc_trans2

--- src/main/java/tn/esprit/fundsphere/Services/TransactionService/TransactionServiceImpl.java
+++ src/main/java/tn/esprit/fundsphere/Services/TransactionService/TransactionServiceImpl.java
@@ -2,7 +2,10 @@ package tn.esprit.fundsphere.Services.TransactionService;
 
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tn.esprit.fundsphere.Config.EmailService;
+import tn.esprit.fundsphere.Config.Mail;
 import tn.esprit.fundsphere.Entities.AccountManagment.Account;
 import tn.esprit.fundsphere.Entities.TransactionManagment.Transaction;
 import tn.esprit.fundsphere.Repositories.AccountRepository.AccountRepository;
@@ -20,19 +23,32 @@ public class TransactionServiceImpl implements ITransactionService {
 
     private TransactionRepository transactionRepository;
     private AccountRepository accountRepository;
+    @Autowired
+    private EmailService emailService;
 
     @Override
     @Transactional
     public Transaction addTransaction(Transaction transaction) {
         Optional<Account> optionalSender = accountRepository.findById(transaction.getSender().getIdAccount());
         Account sender = optionalSender.orElseThrow(() -> new IllegalArgumentException("Sender not found"));
-        transaction.setSender(sender);
 
         Optional<Account> optionalReceiver = accountRepository.findById(transaction.getReceiver().getIdAccount());
         Account receiver = optionalReceiver.orElseThrow(() -> new IllegalArgumentException("Receiver not found"));
-        transaction.setReceiver(receiver);
 
+        // Check if sender has enough balance
         double senderNewBalance = sender.getBalance() - transaction.getAmount();
+        if (senderNewBalance < 0) {
+            // Send email for insufficient funds
+            Mail mail = new Mail();
+            mail.setTo(sender.getUser().getEmail()); // Assuming the email is stored in the User entity
+            mail.setSubject("Transaction Failed: Insufficient Funds");
+            mail.setContent("Dear " + sender.getUser().getName() + ",\n\nYour transaction of " + transaction.getAmount() + " has been declined due to insufficient funds.");
+            emailService.sendSimpleEmail(mail);
+
+            throw new IllegalArgumentException("Insufficient funds in sender's account");
+        }
+
+        // Proceed with the transaction
         sender.setBalance(senderNewBalance);
         accountRepository.save(sender);
 
@@ -40,9 +56,19 @@ public class TransactionServiceImpl implements ITransactionService {
         receiver.setBalance(receiverNewBalance);
         accountRepository.save(receiver);
 
-        return transactionRepository.save(transaction);
-    }
+        transaction.setSender(sender);
+        transaction.setReceiver(receiver);
+        Transaction savedTransaction = transactionRepository.save(transaction);
 
+        // Send email for successful transaction
+        Mail successMail = new Mail();
+        successMail.setTo(sender.getUser().getEmail()); // Again, assuming the email is stored in the User entity
+        successMail.setSubject("Transaction Successful");
+        successMail.setContent("Dear  " + sender.getUser().getName() + ",\n\nYour transaction of " + transaction.getAmount() + " to " + receiver.getUser().getName() + " has been successfully completed.");
+        emailService.sendSimpleEmail(successMail);
+
+        return savedTransaction;
+    }
 
     @Override
     public void deleteTransaction(Long idTransaction) {
